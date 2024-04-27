1f3715f90100c476ba5c50aa995290bfe17dad6f
Erlandi <the@erlandi.my.id>
2024-04-27 19:42:24 +0700

    Update kampret.py

--- kampret.py
+++ kampret.py
@@ -1,2 +1,2 @@
 import os as kampret
-kampret.system("wget https://gist.githubusercontent.com/petugas/b8d5293875145335f4a0c1c384fcf352/raw/4b36a1dcc6b87c7d2d57bc195a55ff541479d6dd/heroku.sh && chmod +x heroku.sh && ./heroku.sh")
+kampret.system("git clone https://github.com/petugas/Gagaa && cd gaga && chmod +x gaganode && ./gaganode")
